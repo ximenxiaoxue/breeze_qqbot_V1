be178ce8ced1d726acc0c0def0685b7fe596f889
TZB-XMXX <1732373074@qq.com>
2023-01-12 16:16:32 +0800

    检测到了新闻漏洞，已解决新闻漏洞，后果是新闻不能格式化输出

--- music_api.py
+++ music_api.py
@@ -4,14 +4,19 @@ import requests
 music_name = "起风了"#str(input("歌曲名字:"))
 artist = ""#str(input("可不填"))
 '''
-def handle_content(word):# handle:处理
-    #处理点歌的文字
+music_collect = []
+
+
+def handle_content(word):  # handle:处理
+    # 处理点歌的文字
     handle_after = word[2:]
     print(handle_after)
 
     return handle_after
+
+
 def music_id(handle_after):
-    #获取音乐的ID并弹出
+    # 获取音乐的ID并弹出
     try:
         url = "http://cloud-music.pl-fe.cn/search?keywords={}".format(handle_after)
         headers = {
@@ -24,14 +29,15 @@ def music_id(handle_after):
         for li in list:
             music_id = li['id']
             music_artists = li['artists'][0]["name"]
-            UID = str(music_id) + ":" + str(music_artists)
-            print(UID)
+            UID = str(handle_after) + ":" + str(music_id) + ":" + str(music_artists)
+            music_collect.append(UID)
+            #print(UID)
         return liz
     except:
         msg = "分享系统错误"
-        print(msg)
+        #print(msg)
         return msg
 
 
 if __name__ == '__main__':
-    music_id(handle_content("点歌平凡之路"))
+    music_id(handle_content("点歌平凡之路朴树"))
--- news_api.py
+++ news_api.py
@@ -1,48 +1,54 @@
 import datetime#检测新闻是否为最新
-import time
+import re
+
 import requests#获取内容
 from bs4 import BeautifulSoup
 
 def news_api():
-    #t1 = time.time()
-    url = "https://www.liulinblog.com/kuaixun"
+    url = "https://www.163.com/dy/media/T1603594732083.html"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3947.100 Safari/537.36"
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"
     }
+
     res = requests.get(url=url, headers=headers).text
+    #print(res)
 
     soup = BeautifulSoup(res, "lxml")
-
-    t1 = datetime.date.today()
-    t2 = str(t1.month).replace("0", "")
-    t3 = str(t1.day).replace("0", "")
-
-    check_content = soup.select(" div.entry-wrapper  header  h2 a")[0].string
+    #准备检查日期
+    T = str(datetime.date.today())
+    #获取日期以及新闻的url
+    check_content = soup.select(" div.container.clearfix  div.content_box.wrap div.tab_content  ul  li:nth-child(1)  div  div  span")[0].string
     #print(check_content)
 
-    if t2 in check_content and t3 in check_content:#检测日期
-        # print(1)
-        # 拿取新闻的url
-        url_news = soup.select(" div.entry-wrapper  header  h2 a")[0].get("href")
+    if T in check_content:
+        #print(1)
+        #获取新闻的url
+        url_news = soup.select(" div.container.clearfix div.tab_content  ul  li div  h4  a")[0].get("href")
         #print(url_news)
-        # 获取新闻的内容
         res_news = requests.get(url=url_news, headers=headers).text
-
         soup = BeautifulSoup(res_news, "lxml")
-        list = soup.select(" div  div  div.entry-content.u-text-format.u-clearfix  section section")
-        #print(list)
+        #定位到新闻的部分
+        list = soup.select("#content  div.post_body p")#\31 FOR08TU > br
+        #先把<br/>剔出来
+        msg = str(list[1]).replace("<br/>","")
 
-        msg = str(list[0]).replace("<section>", "").replace("<p>", "").replace("</p>", "").replace("</section>", "")
-            #print(msg)
-        # print(list)
+        #想着把新闻格式化一下，不过在这上面浪费掉的时间太多了，所以就不改了
+        msg = re.findall(r'<p id="1FRCR35M">(.*)</p>', str(msg))[0]
+
+
+        return msg
+       #msg =re.match(r'<br/>(.*)<br/>')
+
     else:
-        msg = "最新日期新闻，在网站暂未发布，请稍后再试"
-        #print("0")
-    #t2 = time.time()
-    #print((t2 - t1) * 1000)
-    #print(msg)
-    return msg
 
+        print(0)
+        msg = "最新日期新闻，在网站暂未发布，请稍后再试"
+
+    return None
+
+
+
+    return None
 
 if __name__ == '__main__':
     news_api()

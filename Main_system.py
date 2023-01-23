7cd794f4ec8baf24f6aa8a2ec9f2b4f1391e771c
TZB-XMXX <1732373074@qq.com>
2023-01-23 19:04:46 +0800

    增加部分注释，当然多群喊话暂时失效了

--- Main_system.py
+++ Main_system.py
@@ -21,37 +21,45 @@ from api import multi_group_shouting_api #多群喊话的api文件
 from api import kill_pid_system #linux进程保护
 from api import news_api
 
+num = 0
 def Chat_reception():
-    t1 = time.time()
+    global num
+    try:
+        t1 = time.time()
 
-    # 获取消息
-    words = main_api_program.Listener().Preprocessing_segment(main_api_program.Listener().receiver())
-    #print(words)
-    #分离消息
-    Other_chat = main_api_program.Detach_Message().msg_separation(words)
-    if Other_chat == False:
-        pass
-    else:
-        # 输出消息内容 输出的消息是获取的消息
-        sum_1 = threading.Thread(
-            target=main_api_program.Send_operation().Send_operation_first())
-        sum_1.start()
-        #print(1)
-        # 获得回答逻辑中的程序回答
-        word = main_api_program.answer_logic().get_answer()
-        #print(2)
-        # 多群喊话
-        multi_group_shouting_api.multi_group_shouting(word)
-        # 输出的消息是回答逻辑中的程序回答
-        sum_2 = threading.Thread(
-            target=main_api_program.Send_operation().Send_operation_second(word))  # 多群喊话完后这里也会输出，不过是空
-        sum_2.start()
-        # 清除字典里的内容
-        main_api_program.Clear_().clear_Dictionary_receive()
+        # 获取消息
+        words = main_api_program.Listener().Preprocessing_segment(main_api_program.Listener().receiver())
+        # print(words)
+        # 分离消息
+        Other_chat = main_api_program.Detach_Message().msg_separation(words)
+        if Other_chat == False:
+            pass
+        else:
+            # 输出消息内容 输出的消息是获取的消息
+            sum_1 = threading.Thread(
+                target=main_api_program.Send_operation().Send_operation_first())
+            sum_1.start()
+            # print(1)
+            # 获得回答逻辑中的程序回答
+            word = main_api_program.answer_logic().get_answer()
+            # print(2)
+            # 多群喊话
+            multi_group_shouting_api.multi_group_shouting(word)
+            # 输出的消息是回答逻辑中的程序回答
+            sum_2 = threading.Thread(
+                target=main_api_program.Send_operation().Send_operation_second(word))  # 多群喊话完后这里也会输出，不过是空
+            sum_2.start()
+            # 清除字典里的内容
+            main_api_program.Clear_().clear_Dictionary_receive()
 
-    t2 = time.time()
-    print(str(t2 - t1)[:4] + "秒")
+        t2 = time.time()
+        print(str(t2 - t1)[:4] + "秒")
 
+    except:
+        num = num+1
+        print("*****错误*****")
+        if num == 6:
+            kill_pid_system
 
 def pretreatment():
     news_api.news_content()
